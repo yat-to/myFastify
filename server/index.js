b9c53322c25c6578a87dac925ad70bf93b4093f4
yat-to <mhidayatdarmawan@gmail.com>
2026-03-25 00:05:23 +0800

    apply pino-pretty

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({
+    logger: {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid,hostname', // Sembunyikan ID proses biar ringkas
+            },
+        },
+    },
+})
 
 fastify.register(require('./plugins/db'))
 fastify.register(require('./plugins/auth'))
@@ -7,12 +17,12 @@ fastify.register(require('./plugins/auth'))
 fastify.register(require('./routes'))
 
 const start = async () => {
-  try {
-    await fastify.listen({port: 5000})
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+    try {
+        await fastify.listen({ port: 5000 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
 }
 
 start()
